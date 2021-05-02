1a5740fb33e6fd4439f7b6f6738fa15d0f22f841
mihribancaprak <mihribancaprakk@gmail.com>
2021-05-02 21:27:39 +0300

    intro

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,54 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 String internetSubeButonu="Ýnternet Þubesi";
+	       double dolarDun =8.16;
+	       double dolarBugun=8.16;
+	       int vade=36;
+	       boolean durum = true;
+	       
+	       System.out.println(internetSubeButonu);
+	       
+	        if(dolarBugun < dolarDun) {
+	     	  System.out.println("Dolar düþtü resmi"); 
+	        }else if (dolarBugun > dolarDun){
+	     	   System.out.println("Dolar yükseldi resmi");
+	        }else {
+	     	   System.out.println("Dolar eþittir resmi");
+	        }
+	        
+
+	       String[] krediler = {"Hýzlý Kredi","Mutlu Emekli Kredisi","Konut Kredisi"};
+	       
+//	       for(String kredi:krediler) {
+//	    	   System.out.println(kredi);
+//	       }
+	       
+	       for(int i=0;i<krediler.length;i++) {
+	    	   System.out.println(krediler[i]);
+	       }
+	       
+	       int sayi1=10;
+	       int sayi2=20;
+	       sayi1=sayi2;
+	       sayi2=100;
+	       System.out.println(sayi1);
+	       
+	       int[] sayilar1= {1,2,3,4,5};
+	       int[] sayilar2= {10,20,30,40,50};
+	       sayilar1 = sayilar2;
+	       sayilar2[0]=100;
+	       System.out.println(sayilar1[0]);
+	       
+	       String sehir1="Ankara";
+	       String sehir2="Ýstanbul";
+	       sehir1=sehir2;
+	       sehir2="Ýzmir";
+	       System.out.println(sehir1);
+	        
+	}
+
+}
